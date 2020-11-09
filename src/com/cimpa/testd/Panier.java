f2dc6bac5dd3df98774d67f1cbfbe0702600ac9d
gsalandrecimpa <74194179+gsalandrecimpa@users.noreply.github.com>
2020-11-09 18:43:53 +0100

    Corrections sonar smells

--- src/com/cimpa/testd/Panier.java
+++ src/com/cimpa/testd/Panier.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 
 public class Panier {
 	private int contenance;
-	private ArrayList<Orange> panier = new ArrayList<>();
+	private ArrayList<Orange> contenu = new ArrayList<>();
 	
 	public Panier(int contenance) throws PanierException {
 		setContenance(contenance);
@@ -16,43 +16,43 @@ public class Panier {
 	}
 	public void setContenance(int contenance) throws PanierException {
 		if (contenance < 1) throw new PanierException("La contenance doit être d'au moins 1");
-		if (contenance < panier.size()) throw new PanierException(
+		if (contenance < contenu.size()) throw new PanierException(
 				"Une nouvelle contenance ne peut être inférieure à la taille courante (="
-						+panier.size()+") du panier.");
+						+contenu.size()+") du panier.");
 		this.contenance = contenance;
 	}
 	
 	public boolean estPlein() {
-		return panier.size() == contenance;
+		return contenu.size() == contenance;
 	}
 	
 	public boolean estVide() {
-		return panier.size() == 0;
+		return contenu.isEmpty();
 	}
 	
 	public void ajoute(Orange o) throws PanierException {
 		if (o == null) throw new PanierException("ajout impossible d'un element null");
 		if (estPlein()) throw new PanierException("ajout impossible car le panier est plein");
-		if (!panier.add(o)) throw new PanierException("echec ajout, retour false de la methode add");
+		if (!contenu.add(o)) throw new PanierException("echec ajout, retour false de la methode add");
 	}
 	
 	public void retire() {
-		if (!panier.isEmpty()) {
-			panier.remove(panier.size()-1);
+		if (!contenu.isEmpty()) {
+			contenu.remove(contenu.size()-1);
 		}
 	}
 	
 	public double getPrix() {
 		double prix = 0;
-		for (Orange o:panier) prix+=o.getPrix();
+		for (Orange o:contenu) prix+=o.getPrix();
 		return prix;
 	}
 	
 	public void boycotteOrigine(String bo) {
 		int i;
-		for (i=0;i<panier.size();) {
-			if (Objects.equals(panier.get(i).getOrigine(),bo)) 
-				panier.remove(i);
+		for (i=0;i<contenu.size();) {
+			if (Objects.equals(contenu.get(i).getOrigine(),bo)) 
+				contenu.remove(i);
 			else 
 				i++;
 		}
@@ -61,9 +61,13 @@ public class Panier {
 	
 	@Override
 	public String toString() {
-		String ret = String.format("Panier-prix total=%.02f:",getPrix());
-		for (Orange o:panier) ret+=o.toString()+";";
-		return ret;
+		StringBuilder bld = new StringBuilder();
+		bld.append(String.format("Panier-prix total=%.02f:",getPrix()));
+		for (Orange o:contenu) {
+			bld.append(o.toString());
+			bld.append(';');
+		}
+		return bld.toString();
 	}
 	
 	@Override
@@ -71,7 +75,7 @@ public class Panier {
 		if (obj instanceof Panier) {
 			Panier p = (Panier) obj;
 			return contenance == p.contenance
-					&& Objects.equals(panier, p.panier);
+					&& Objects.equals(contenu, p.contenu);
 		}
 		else return false;
 	}
